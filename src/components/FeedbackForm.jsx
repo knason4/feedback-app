1ac67a64e440398814d8b8437c89ba56a275d330
knason4 <knason4@gmail@github.com>
2022-05-30 16:18:29 -0500

    commit form and button components

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,7 +1,31 @@
+import Card from "./shared/Card"
+import {useState} from 'react'
+import Button from './shared/Button'
 
 function FeedbackForm() {
+    const [text, setText] = useState('')
+    const handleTextChange = (e) => {
+        setText(e.target.value)
+    }
+
   return (
-    <div>Form</div>
+      <Card>
+        <form>
+            <h2>How would you rate your service with us?</h2>
+            {/* @todo - rating select component */}
+            <div className="input-group">
+                <input 
+                    onChange={handleTextChange} 
+                    type='text' 
+                    placeholder="Write a review"
+                    value={text}
+                />
+                <Button type='submit' version='secondary'>Send</Button>
+            </div>
+        </form>
+
+      </Card>
+    
   )
 }
 
