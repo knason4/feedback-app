430b3a322cf6b32f0a2712dba168ff36d5c1c743
knason4 <knason4@gmail@github.com>
2022-05-30 15:38:42 -0500

    changes up through section 3

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,28 @@
+import {FaTimes} from 'react-icons/fa'
+import PropTypes from 'prop-types'
+import Card from './shared/Card'
+
+function FeedbackItem({item, handleDelete}) {
+    
+
+/*    const [rating, setRating] = useState(7)
+    const [text, setText] = useState('This is an example of a feedback item')
+*/  
+   
+    
+    return (
+        <Card >
+            <div className="num-display">{item.rating}</div>
+            <button onClick={() => handleDelete(item.id)} 
+            className='close'>
+                <FaTimes color='purple' />
+            </button>
+            <div className="text-display">{item.text}</div>
+        </Card>
+  )
+}
+
+FeedbackItem.propTypes = {
+    item: PropTypes.object.isRequired
+}
+export default FeedbackItem
---
+++ src/components/shared/Card.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types'
+function Card({children, reverse}) {
+/* 
+This is a conditional class
+return (
+    <div className={`card ${reverse && 'reverse'}`}>{children}</div>
+  )
+  */
+
+  // this is a conditional style
+    return ( 
+    <div 
+        className="card" 
+        style={{
+            backgroundColor: reverse ? 'rgba(0,0,0,0.4)' : '#fff',
+            color: reverse ? '#fff' : '#000',
+        }}
+    >
+        {children}
+    </div>
+    )
+}
+// ${} is a template string, inside is an if then statement,
+// if reverse is true, then return the class of reverse
+
+Card.defaultProps = {
+    reverse: true,
+}
+
+Card.propTypes = {
+    children: PropTypes.node.isRequired,
+    reverse: PropTypes.bool,
+}
+export default Card
